7649e6f60a670f43f0f804e168d46aa33bb9165a
Juan Berrios <jberrioscifuentes@gmail.com>
2021-02-19 18:17:46 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+      '3xl': '1536px'
+    },
+    fontFamily: {
+      'text': ["Fivo Sans", 'sans-serif']
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.coolGray,
+      black: colors.black,
+      white: colors.white,
+      blue: {
+        light: '#2B4965',
+        DEFAULT: '#002241'
+      },
+      orange: {
+        DEFAULT: '#FF6633'
+      },
+      green: {
+        DEFAULT: '#2DBC3B',
+        dark: '#28A634'
+      },
+      red: {
+        DEFAULT: '#FF0000',
+        dark: '#DE0000'
+      }
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
