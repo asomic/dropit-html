3e32bf3e03f19d261e0bc8c54ba3ad086e67491e
dandelior <jberrioscifuentes@gmail.com>
2021-03-15 15:23:29 -0300

    cambios editModal (d'work)

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -12,24 +12,31 @@
             }
         )
     
+        let body = document.querySelector('body');
         let closeModalEditar = document.querySelector('#closeModalEditar');
         let closeModalChat = document.querySelector('#closeModalChat');
+        let closeModalClient = document.querySelector('#closeModalClient');
+
         let buttonEditOrder = document.querySelector('#buttonEditOrder');
         let buttonOpenChat = document.querySelector('#buttonOpenChat');
+        let buttonOpenEditClient = document.querySelector('#buttonOpenEditClient');
         
         let pageModal = document.querySelector('#pageModal');
         let modalEditar = document.querySelector('#modalEditar');
         let modalChat = document.querySelector('#modalChat');
+        let modalClient = document.querySelector('#modalClient');
         
         // Modal Editar
         buttonEditOrder.onclick = () => {
+            body.style.overflow = 'hidden';
             pageModal.classList.remove('invisible');
             pageModal.classList.add('visible');
             modalEditar.classList.remove('invisible');
             modalEditar.classList.add('visible');
         }
-
+        
         closeModalEditar.onclick = () => {
+            body.style.overflow = 'auto';
             pageModal.classList.remove('visible');
             pageModal.classList.add('invisible');
             modalEditar.classList.remove('visible');
@@ -38,17 +45,38 @@
         
         // Modal Chat
         buttonOpenChat.onclick = () => {
+            body.style.overflow = 'hidden';
             pageModal.classList.remove('invisible');
             pageModal.classList.add('visible');
             modalChat.classList.remove('invisible');
             modalChat.classList.add('visible');
         }
-
+        
         closeModalChat.onclick = () => {
+            body.style.overflow = 'auto';
             pageModal.classList.remove('visible');
             pageModal.classList.add('invisible');
             modalChat.classList.remove('visible');
             modalChat.classList.add('invisible');
+        }
+        
+        // Modal Edit Client
+        buttonOpenEditClient.onclick = () => {
+            console.log('opened');
+            // body.style.overflow = 'hidden';
+            // pageModal.classList.remove('invisible');
+            // pageModal.classList.add('visible');
+            // modalClient.classList.remove('invisible');
+            // modalClient.classList.add('visible');
+        }
+        
+        closeModalClient.onclick = () => {
+            console.log('closed');
+            // body.style.overflow = 'auto';
+            // pageModal.classList.remove('visible');
+            // pageModal.classList.add('invisible');
+            // modalClient.classList.remove('visible');
+            // modalClient.classList.add('invisible');
         }
 
     };
