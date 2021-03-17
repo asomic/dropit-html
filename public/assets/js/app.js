6e3fba6faa1ecb2dbfd4c1e70538f5c1ece87405
Juan Berrios <jberrioscifuentes@gmail.com>
2021-03-17 18:38:37 -0300

    more modals

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -17,18 +17,34 @@
         let detailProd = document.querySelectorAll('.openDetailProd');
         let buttonEditOrder = document.querySelector('#buttonEditOrder');
         let buttonOpenChat = document.querySelector('#buttonOpenChat');
+        let buttonOpenChatN = document.querySelectorAll('.buttonOpenChat');
         let buttonOpenEditClient = document.querySelector('#buttonOpenEditClient');
-
+        let buttonModalInfoOrder = document.querySelector('#buttonModalInfoOrder');
+        let openModalEditAttribute = document.querySelectorAll('.openModalEditAttribute');
+        
         let closeModalEditar = document.querySelector('#closeModalEditar');
         let closeModalChat = document.querySelector('#closeModalChat');
         let closeModalClient = document.querySelector('#closeModalClient');
         let closeModalProductDetail = document.querySelector('#closeModalProductDetail');
+        let closeModalInfoOrder = document.querySelector('#closeModalInfoOrder');
         
         let pageModal = document.querySelector('#pageModal');
         let modalProductDetail = document.querySelector('#modalProductDetail');
         let modalEditar = document.querySelector('#modalEditar');
         let modalChat = document.querySelector('#modalChat');
+        let modalChatN = document.querySelector('#modalChatN');
         let modalClient = document.querySelector('#modalClient');
+        let modalInfoOrder = document.querySelector('#modalInfoOrder');
+        let modalEditAttribute = document.querySelector('#modalEditAttribute');
+        
+        // Order Modals
+        let buttonAddCart = document.querySelectorAll('.buttonAddCart');
+
+        let sidebarCart = document.querySelector('#sidebarCart');
+        let modalOrderProductDetail = document.querySelector('#modalOrderProductDetail');
+        let modalOrderAsignClient = document.querySelector('#modalOrderAsignClient');
+        let modalOrderDataClient = document.querySelector('#modalOrderDataClient');
+        let modalOrderResume = document.querySelector('#modalOrderResume');
         
         // Modal Edit Client
         if (modalClient) {
@@ -70,6 +86,7 @@
 
         // Modal Chat
         if (modalChat) {
+
             buttonOpenChat.onclick = () => {
                 body.style.overflow = 'hidden';
                 pageModal.classList.remove('invisible');
@@ -84,6 +101,50 @@
                 pageModal.classList.add('invisible');
                 modalChat.classList.remove('visible');
                 modalChat.classList.add('invisible');
+            }
+
+        }
+
+        if (modalInfoOrder) {
+
+            buttonModalInfoOrder.onclick = () => {
+                body.style.overflow = 'hidden';
+                pageModal.classList.remove('invisible');
+                pageModal.classList.add('visible');
+                modalInfoOrder.classList.remove('invisible');
+                modalInfoOrder.classList.add('visible');
+            }
+
+            closeModalInfoOrder.onclick = () => {
+                body.style.overflow = 'auto';
+                pageModal.classList.remove('visible');
+                pageModal.classList.add('invisible');
+                modalInfoOrder.classList.remove('visible');
+                modalInfoOrder.classList.add('invisible');
+            }
+
+        }
+
+        if (modalChatN) {
+            for (let i = 0; i < buttonOpenChatN.length; i++) {
+
+                buttonOpenChatN[i].onclick = () => {
+                    console.log('clicke');
+                    body.style.overflow = 'hidden';
+                    pageModal.classList.remove('invisible');
+                    pageModal.classList.add('visible');
+                    modalChatN.classList.remove('invisible');
+                    modalChatN.classList.add('visible');
+                }
+                
+            }
+
+            closeModalChat.onclick = () => {
+                body.style.overflow = 'auto';
+                pageModal.classList.remove('visible');
+                pageModal.classList.add('invisible');
+                modalChatN.classList.remove('visible');
+                modalChatN.classList.add('invisible');
             }
         }
 
@@ -102,14 +163,44 @@
             }
 
             closeModalProductDetail.onclick = () => {
-                body.style.overflow = 'hidden';
+                body.style.overflow = 'auto';
                 pageModal.classList.remove('visible');
                 pageModal.classList.add('invisible');
                 modalProductDetail.classList.remove('visible');
                 modalProductDetail.classList.add('invisible');
             }
         }
-        
+
+        if (modalEditAttribute) {
+            for (let step = 0; step < openModalEditAttribute.length; step++) {
+
+                openModalEditAttribute[step].onclick = () => {
+                    // console.log('funca');
+                    body.style.overflow = 'hidden';
+                    pageModal.classList.remove('invisible');
+                    pageModal.classList.add('visible');
+                    modalEditAttribute.classList.remove('invisible');
+                    modalEditAttribute.classList.add('visible');
+                }
+
+            }
+
+            closeModalInfoOrder.onclick = () => {
+                body.style.overflow = 'auto';
+                pageModal.classList.remove('visible');
+                pageModal.classList.add('invisible');
+                modalEditAttribute.classList.remove('visible');
+                modalEditAttribute.classList.add('invisible');
+            }
+
+        }
+
+        // Order Modals actions
+        // if (modalOrderProductDetail) {
+        //     for (let step = 0; step < buttonAddCart.length; step++) {
+
+        //     }
+        // }
 
     };
 
